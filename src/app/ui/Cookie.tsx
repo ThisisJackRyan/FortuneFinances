8169eb28c1538db6b6b0934d4d9fe11d510b6b54
Jack Ryan <thisisjackryan1@gmail.com>
2024-11-08 21:55:18 -0600

    seperate cookie component

---
+++ src/app/ui/Cookie.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cookie: React.FC = () => {
+    return (
+        <div>
+            <p>
+                Cookie Goes Here
+            </p>        
+        </div>
+    );
+};
+
+export default Cookie;
