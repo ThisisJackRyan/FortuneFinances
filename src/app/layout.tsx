b583baa10ec85f58eb8428ef3ff5e8cb80845910
Jack Ryan <thisisjackryan1@gmail.com>
2024-11-09 23:32:49 -0600

    remove unneed fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 const sourGummy = localFont({
   src: "./fonts/SourGummy-VariableFont_wdth,wght.ttf",
   variable: "--font-sour-gummy",
@@ -35,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <UserProvider>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} ${sourGummy.variable} antialiased`}
+          className={`${sourGummy.variable} antialiased`}
           style={{fontFamily: sourGummy.style.fontFamily}}
         >
           {children}
